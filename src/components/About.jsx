6d9ad2fb974c1d518c1b3906fb7ed03c9dfecb4c
Nikhil Srivastava <srivastavanikhil756@gmail.com>
2026-02-16 22:59:15 +0530

    font

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <SectionWrapper>
       <section id="about" className="section">
         <h2>About Me</h2>
-        <p>
+        <p class="about-text">
           I am a passionate software developer with a strong foundation in Object-Oriented Programming and Backend Development. I have completed my MCA from the School of Management Sciences, Varanasi, and I am currently refining my industry-ready skills at QSpiders.
 
 My primary focus is on building scalable web applications using Java, Spring Boot, and REST APIs. I enjoy solving complex problems using data structures and have hands-on experience integrating backend applications with databases using Spring Boot, Hibernate/JPA.
